785d938be8e18bdb0ce30f825f26a053b031921c
Will Cauthen <willcauthen50@gmail.com>
2016-02-08 14:20:41 -0800

    fucking around with angular

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,14 @@
+var app = angular.module("storkclub", []);
+
 console.log("we're in good shape");
+
+app.controller('MainCtrl', function ($scope) {
+	$scope.message = "world";
+
+	$scope.bands = [
+		{name: "The Hospital Bombers", members: 5, genre: "death metal"},
+		{name: "Satan's Fingers", members: 3, genre: "death metal"},
+		{name: "The Best Ever Death Metal Band Out of Denton", members: 2, genre: "death metal"},
+		{name: "Elvis Depressedly", members: 3, genre: "depressing indie shit"}
+	];
+});
